2ba8d0b3bdcf02f0ce17cab11bed926421b54dc7
ifwang <wang.if@qq.com>
2014-09-25 00:43:25 +0800

    Initial Commit

---
+++ DoctorDoc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoctorDoc
+//
+//  Created by ifwang on 14-9-25.
+//  Copyright (c) 2014年 ifwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DoctorDoc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DoctorDoc
+//
+//  Created by ifwang on 14-9-25.
+//  Copyright (c) 2014年 ifwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
